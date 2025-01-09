636e5b8e3e4c44397b1e1b623fb3db90fcee2544
BRF4409A <ferreol.barbara-de-labelotterie-de-boi@univ-tlse3.fr>
2025-01-09 11:55:46 +0100

    TP1 fin de tout

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -14,9 +14,15 @@ public class Gaulois {
 	}
 	
 	public void parler(String parole) {
-		System.out.println("Le Gaulois " + getNom() + " : \"" + parole + "\".");
+		System.out.println("Le Gaulois " + nom + " : \"" + parole + "\".");
 	}
 	
+	public void frapper(Romain romain) {
+		String nomRomain = romain.getNom();
+		System.out.println(nom + " envoie un grand coup dans la mâchoire de " + nomRomain );
+		romain.recevoirCoup(force/3);
+	}
+		
 	public static void main(String[] args) {
 		Gaulois asterix = new Gaulois("Asterix", 8);
 		asterix.parler("Bonjour à tous");
--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -3,13 +3,27 @@ package personnages;
 public class Romain {
 	private String nom;
 	private int force;
-	
-	public Romain(String nom, int force){
+
+	public Romain(String nom, int force) {
 		this.nom = nom;
 		this.force = force;
 	}
-	
+
 	public String getNom() {
 		return nom;
+	}
+
+	public void parler(String parole) {
+		System.out.println("Le Romain " + nom + " : \"" + parole + "\".");
+	}
+
+	public void recevoirCoup(int forceCoup) {
+		force = force - forceCoup;
+		if (force > 0) {
+			System.out.println("Aïe!");
+		} else {
+			force = 0;
+			System.out.println("J’abandonne…");
+		}
 	}
 }
